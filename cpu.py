679bdb3f9813dd951364241590557395cd5ffc73
christiannshuti <chrisnshuti943@gmail.com>
2026-08-05 08:48:25 +0200

    First commit after making the CPU class with a constructor and also a load program method and also made main.py that contains the program to be run

---
+++ cpu.py
@@ -0,0 +1,19 @@
+class TinyCPU:
+    def __init__(self):
+        self.registers = {
+            "R1": 0,
+            "R2": 0,
+            "R3": 0,
+            "R4": 0,
+        }
+
+        self.memory = []
+        self.pc = 0
+        self.running = True
+
+
+    def load_program(self,program):
+        self.memory = program
+        self.pc = 0
+        self.running = True
+        
---
+++ main.py
@@ -0,0 +1,7 @@
+program = [
+    "LOAD R1 5",
+    "LOAD R2 10",
+    "ADD R1 R2",
+    "PRINT R1",
+    "HALT"
+]
