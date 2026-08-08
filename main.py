9b82fcfa668e60fbeaf1a173f178e54428ac37e1
christiannshuti <chrisnshuti943@gmail.com>
2026-08-08 17:03:50 +0200

    Modified main to run the cpu and execute the program

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from cpu import TinyCPU
+
+cpu = TinyCPU()
+
 program = [
     "LOAD R1 5",
     "LOAD R2 10",
     "ADD R1 R2",
     "PRINT R1",
+    "SUB R1 R2",
+    "PRINT R1",
     "HALT"
 ]
+
+cpu.load_program(program)
+cpu.run()
+
