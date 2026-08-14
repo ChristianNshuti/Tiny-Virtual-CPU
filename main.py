996d69830c699b171ca43f1d97a83aa126dd522b
christiannshuti <chrisnshuti943@gmail.com>
2026-08-14 19:45:21 +0200

    Tried implementing multiply instruction of (10 * 3) + (20 * 2) using our few instructions we only had which are ADD,SUB,PRINT,HALT and LOAD

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from cpu import TinyCPU
 cpu = TinyCPU()
 
 program = [
-    "LOAD R1 7",
-    "LOAD R2 8",
+    "LOAD R1 10",
+    "LOAD R2 10",
     "ADD R1 R2",
-    "LOAD R3 4",
-    "LOAD R4 6",
-    "ADD R3 R4",
-    "SUB R1 R3",
+    "ADD R1 R2",
+    "LOAD R3 20",
+    "ADD R3 R3",
+    "ADD R1 R3",
     "PRINT R1",
     "HALT"
 ]
