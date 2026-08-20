8e75cce5787aaab835d1049ad3cf61d741954b40
christiannshuti <chrisnshuti943@gmail.com>
2026-08-20 08:11:01 +0200

    Made the first while loop that loops as long as R1 < 5

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from cpu import TinyCPU
 cpu = TinyCPU()
 
 program = [
-    "LOAD R1 10",          
-    "LOAD R2 5",          
-    "CMP R1 R2",           
-    "JUMP_IF_LESS 6",      
-    "JUMP_IF_ZERO 8",      
-    "JUMP_IF_GREATER 10",  
-    "PRINT R1",            
-    "HALT",                
-    "PRINT R2",            
-    "HALT",                
-    "PRINT R1",            
-    "HALT"                 
+    "LOAD R1 0",
+    "LOAD R2 5",
+    "LOAD R3 1",
+    "CMP R1 R2",
+    "JUMP_IF_LESS 5",
+    "JUMP_IF_ZERO 9",
+    "PRINT R1",
+    "ADD R1 R3",
+    "JUMP 3",        
+    "HALT"     
 ]
 
 cpu.load_program(program)
