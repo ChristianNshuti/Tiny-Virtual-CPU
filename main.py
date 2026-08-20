90929afab44c624954f322fd552f11b20ab31582
christiannshuti <chrisnshuti943@gmail.com>
2026-08-20 08:03:09 +0200

    Added a program that uses the new instructions which are JUMP_IF_LESS,JUMP_IF_ZERO and JUMP_IF_GREATER

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ from cpu import TinyCPU
 cpu = TinyCPU()
 
 program = [
-    "LOAD R1 5",
-    "LOAD R2 10",
-    "CMP R1 R2",
-    "JUMP_IF_LESS 6",
-    "PRINT R1",
-    "HALT",
-    "PRINT R2",
-    "HALT"
+    "LOAD R1 10",          
+    "LOAD R2 5",          
+    "CMP R1 R2",           
+    "JUMP_IF_LESS 6",      
+    "JUMP_IF_ZERO 8",      
+    "JUMP_IF_GREATER 10",  
+    "PRINT R1",            
+    "HALT",                
+    "PRINT R2",            
+    "HALT",                
+    "PRINT R1",            
+    "HALT"                 
 ]
 
 cpu.load_program(program)
