e4b55604801528970ce3580cb5909c4d3a7b6947
christiannshuti <chrisnshuti943@gmail.com>
2026-08-22 21:27:17 +0200

    feat:Made the execute function and moved all execute conditions into the execute function

--- cpu.py
+++ cpu.py
@@ -24,80 +24,13 @@ class TinyCPU:
 
     def run(self):
         while self.running and self.pc < len(self.memory) :
-
             # Fetch
             instruction = self.fetch()
-
             # Decode
             parts = self.decode(instruction)
-
             # Execution comes next
-            opcode = parts[0]
-            if opcode == "LOAD":
-                register = parts[1]
-                value = int(parts[2])
-                self.registers[register] = value
+            self.execute(parts)
 
-            elif opcode == "ADD":
-                register1 = parts[1]
-                register2 = parts[2]
-                self.registers[register1] += self.registers[register2]
-
-            elif opcode == "SUB":
-                register1 = parts[1] 
-                register2 = parts[2]
-                self.registers[register1] -= self.registers[register2]
-
-            elif opcode == "JUMP":
-                self.pc = int(parts[1])
-                continue
-
-            elif opcode == "JUMP_IF_LESS":
-                if self.flags["LESS"]:
-                    self.pc = int(parts[1])
-                    continue
-
-            elif opcode == "JUMP_IF_ZERO":
-                if self.flags["ZERO"]:
-                    self.pc = int(parts[1])
-                    continue
-
-            elif opcode == "JUMP_IF_GREATER":
-                if self.flags["GREATER"]:
-                    self.pc = int(parts[1])
-                    continue
-
-            elif opcode == "PRINT":
-                register = parts[1]
-                value = self.registers[register]
-                print(value)
-
-
-            elif opcode == "CMP":
-                reg1 = parts[1]
-                reg2 = parts[2]
-                register1 = self.registers[reg1]
-                register2 = self.registers[reg2]
-
-                self.flags["LESS"] = False
-                self.flags["ZERO"] = False
-                self.flags["GREATER"] = False
-
-                if register1 < register2:
-                    self.flags["LESS"] = True
-
-                elif register1 == register2:
-                    self.flags["ZERO"] = True
-
-                elif register1 > register2:
-                    self.flags["GREATER"] = True
-
-            elif opcode == "HALT":
-                self.running = False
-
-
-            #Move to next instruction
-            self.pc += 1
 
     def fetch(self):
         return self.memory[self.pc]
@@ -105,4 +38,69 @@ class TinyCPU:
     def decode(self, instruction):
         return instruction.split()
     
-    
+    def execute(self,parts):
+        opcode = parts[0]
+        if opcode == "LOAD":
+            register = parts[1]
+            value = int(parts[2])
+            self.registers[register] = value
+
+        elif opcode == "ADD":
+            register1 = parts[1]
+            register2 = parts[2]
+            self.registers[register1] += self.registers[register2]
+
+        elif opcode == "SUB":
+            register1 = parts[1] 
+            register2 = parts[2]
+            self.registers[register1] -= self.registers[register2]
+
+        elif opcode == "JUMP":
+            self.pc = int(parts[1])
+            return
+
+        elif opcode == "JUMP_IF_LESS":
+            if self.flags["LESS"]:
+                self.pc = int(parts[1])
+                return
+
+        elif opcode == "JUMP_IF_ZERO":
+            if self.flags["ZERO"]:
+                self.pc = int(parts[1])
+                return
+
+        elif opcode == "JUMP_IF_GREATER":
+            if self.flags["GREATER"]:
+                self.pc = int(parts[1])
+                return
+
+        elif opcode == "PRINT":
+            register = parts[1]
+            value = self.registers[register]
+            print(value)
+
+        elif opcode == "CMP":
+            reg1 = parts[1]
+            reg2 = parts[2]
+            register1 = self.registers[reg1]
+            register2 = self.registers[reg2]
+
+            self.flags["LESS"] = False
+            self.flags["ZERO"] = False
+            self.flags["GREATER"] = False
+
+            if register1 < register2:
+                self.flags["LESS"] = True
+
+            elif register1 == register2:
+                self.flags["ZERO"] = True
+
+            elif register1 > register2:
+                self.flags["GREATER"] = True
+
+        elif opcode == "HALT":
+            self.running = False
+
+        #Move to next instruction
+        self.pc += 1
+     
